2e6b856292c54e5b7aa8f99652aefd2426ea67ee
Ed Knueppel <edward.knueppel@zywave.com>
2021-11-27 18:07:10 -0600

    adds labels to charts

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 import requests 
 import numpy as np
-import pandas as pd, matplotlib.pyplot as plt, statsmodels.api as sm
+import pandas as pd, matplotlib.pyplot as plt
+import seaborn as sns
 
 response = requests.get("https://github.com/kalilurrahman/TCSData/blob/main/TCS_stock_history.csv")
-#print(response.text)
-table_data = pd.read_html(response.text)
-df = table_data[0]
-df.head()
-print(df) 
+table_data = pd.read_html(response.text, header=0)
+df_full = table_data[0] #first in a list of DataFrame objects
+
+df = df_full.iloc[1:100]
 
 df.plot(x = "Date", y = "Close")
 plt.title("Close Price")
-plt.ylabel("price")
-plt.figure()
-#plt.show()
-#y_axis = np.array(df['Dividends'])
-#x_axis = np.array(df['Date'])
-#df.plot(x = "Date", y = "Dividends")
-#d = np.polyfit(x_axis, y_axis, 1)
-#f = np.poly1d(d)
-#df.insert(6, 'Treg', f(df['Date']))
-#ax = df.plot(x = 'Date', y = 'Dividends')
-#df.plot(x = 'Date', y = 'Treg', color = 'Red', ax= ax)
-#plt.show()
-#df2 = df
-df.plot(x = "Date", y = "Volume")
-df.plot.bar()
-plt.figure()
+plt.ylabel("Price")
+
+df.plot(x = "Date", y = "Volume", kind="bar")
+plt.title("Volume")
+plt.ylabel("Volume")
+
+sns.lmplot(x='Close',y='Volume', data=df, fit_reg=True)
+plt.title("Volume - Scatter Plot w/ Linear Regrassion")
+plt.ylabel("Volume")
+
 plt.show()
